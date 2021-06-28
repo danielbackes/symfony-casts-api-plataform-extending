933f365571ba9e032a426077036fd6ec65718454
Daniel Backes <danielbackes@protonmail.com>
2021-06-28 19:35:05 -0400

    feat: starter project

---
+++ src/DataPersister/UserDataPersister.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DataPersister;
+
+use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserDataPersister implements DataPersisterInterface
+{
+    private $decoratedDataPersister;
+    private $userPasswordEncoder;
+
+    public function __construct(DataPersisterInterface $decoratedDataPersister, UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->decoratedDataPersister = $decoratedDataPersister;
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    public function supports($data): bool
+    {
+        return $data instanceof User;
+    }
+
+    /**
+     * @param User $data
+     */
+    public function persist($data)
+    {
+        if (!$data->getId()) {
+            $this->logger->info(sprintf('User %s just registered! Eureka!', $data->getEmail()));
+        }
+        
+        if ($data->getPlainPassword()) {
+            $data->setPassword(
+                $this->userPasswordEncoder->encodePassword($data, $data->getPlainPassword())
+            );
+            $data->eraseCredentials();
+        }
+
+        $this->decoratedDataPersister->persist($data);
+    }
+
+    public function remove($data)
+    {
+        $this->decoratedDataPersister->remove($data);
+    }
+}
